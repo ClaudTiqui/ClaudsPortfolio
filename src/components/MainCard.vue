fd4bc44944b63d35720493273aac86339c667452
Claud Tiqui <claud.tiqui03@gmail.com>
2023-04-23 16:32:29 -0400

    Started Portfolio

---
+++ src/components/MainCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="main-container">
+    <div class="container-one">
+        <div class="profile">
+            <img src="../assets/profilePicture.jpg" alt="">
+            <h3>Claud Tiqui</h3>
+            <p>Software Developer</p>
+        </div>
+        <div class="social-links">
+            <a href="">
+                <div class="link">
+                    <img src="../assets/github.png" alt="">
+                    <h2>GitHub</h2>
+                </div>
+                <div class="link">
+                    <img src="../assets/linkedIn.png" alt="">
+                    <h2>LinkedIn</h2>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="container-two">
+        <div class="about-me">
+            <h1>About Me</h1>
+            <p>This is test paragraph</p>
+        </div>
+        <div class="technology-section">
+            <h1>Technologies Studied</h1>
+            <div class="logos">
+                <img src="" alt="">
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    img{
+        height: 20px;
+    }
+</style>
